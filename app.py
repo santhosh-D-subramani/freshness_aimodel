8c1dddac5c52cf8ad45310e9b6d3dac88a79ae65
Santhosh Subramani <134263756+santhosh-D-subramani@users.noreply.github.com>
2024-02-25 11:35:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def predict_mask_route():
         return jsonify({'error': 'Internal server error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
